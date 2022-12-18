8f30864eec81336f9a32e83b72d38ed59d445d7c
Nicolas Seznec <seznec.nicolas@yahoo.fr>
2022-12-18 10:08:06 +0100

    Added main tasks to Main

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,23 @@ LOCALHOST = "127.0.0.1"
 
 
 def main():
+    # Initialization
+    # -------- 1. Get the list of Nodes ----------
+    # (use a directory authority ?)
+    # Save list ? Or retrieve again each time
+    # Register the new node as part of relays
+
+    # -------- 2. Establish the circuit ----------
+    # Select the guard, middle and exit node
+    # Build the circuit step by step, retreiving the key of each node
+
+    # -------- 3. Send message -------------------
+    # encrypt in layers
+    # send to guard node
+
+    # -------- 4. Recieve message ----------------
+    # Recieve encrypted message
+    # decrypt layers
     pass
 
 
