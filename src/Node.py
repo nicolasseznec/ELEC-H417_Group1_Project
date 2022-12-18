0f7e31d422ca44da3670651d803fd5c8c5c38f41
$Mohcine2001 <$mohamoumi01@hotmail.com>
2022-12-18 13:17:08 +0100

    Implementation of encryption

--- src/Node.py
+++ src/Node.py
@@ -1,6 +1,8 @@
 import json
 import random
 import time
+import rsa
+from random import randrange
 
 from ConnectionsThread import *
 from src.ConnectionsThread import NodeServerThread
@@ -21,7 +23,8 @@ class Node:
         # Message first sent from this Node
         self.self_messages = {}
 
-        self.key = None  # To be done
+        self.keyPublic, self.keyPrivate = rsa.newkeys(512) # Generate the public and the private key
+        self.listNodes = [] #each element of the liste is a tuple (addresse, port, publicKey)
 
         self.connection = NodeServerThread(self)
 
@@ -45,12 +48,52 @@ class Node:
         message = {**dico, **overload}
         return message
 
+    def encrypt_message(self, msg, path_list):
+        encrypt1 = json.dumps(msg)
+        encrypt1 = rsa.encrypt(encrypt1.encode("utf-8"),path_list(2)(2))  # path_list(2)(2) donne la publicKey de exitNode
+        encrypt2 = {"receiver" : (path_list(2)(0),path_list(2)(1)), "data" : encrypt1}
+
+        # (path_list(1)(0),path_list(1)(1)) -> donne un tuple avec address et port du prochain hop
+
+        encrypt2 = json.dumps(encrypt2)
+        encrypt2 = rsa.encrypt(encrypt2.encode("utf-8"), path_list(1)(2))
+
+        encrypt3 = {"receiver" : (path_list(1)(0),path_list(1)(1)), "data" : encrypt2}
+        encrypt3 = json.dumps(encrypt3)
+        encrypt3 = rsa.encrypt(encrypt3.encode("utf-8"), path_list(0)(2))
+
+        final_message = encrypt3
+
+        return encrypt3
+
+
+
     def send_message_to(self, data, type, receiver):
         # if receiver :
         #     data.encrypt(receiver.key)        // Example
         overload = {"receiver": receiver}
         message = self.construct_message(data, type, overload)
         self.broadcast_to_network(message)
+
+    def findPath(self):
+        n = len(self.listNodes)
+        liste = []
+        if n < 5:
+            print("Not enough nodes")
+            return
+        else:
+            entryIndex = randrange(n)
+            middleIndex = entryIndex
+            while(middleIndex == entryIndex):
+                middleIndex = randrange(n)
+            exitIndex = entryIndex
+            while(exitIndex == middleIndex or exitIndex == entryIndex):
+                exitIndex = randrange(n)
+            liste.append(self.listNodes(entryIndex))
+            liste.append(self.listNodes(middleIndex))
+            liste.append(self.listNodes(exitIndex))
+            return liste
+
 
     # def broadcast_peers_list(self):
     #     """
